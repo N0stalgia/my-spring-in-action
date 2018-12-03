aef40afb3482001ee094969047a98be5d5af27f6
chenjiaqi <jiaqi.chen@sky-cloud.net>
2018-12-03 22:57:23 +0800

    chore: 初次构建项目

---
+++ src/main/java/net/jack/chen/MySpringInActionApplication.java
@@ -0,0 +1,12 @@
+package net.jack.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringInActionApplication.class, args);
+    }
+}
---
+++ src/test/java/net/jack/chen/BoringTest.java
@@ -0,0 +1,13 @@
+package net.jack.chen;
+
+import org.junit.Test;
+
+/**
+ * Created by CJQ on 2018/12/2.
+ */
+public class BoringTest {
+    @Test
+    public void testGC() {
+        System.out.println("hello World");
+    }
+}
